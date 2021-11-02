f01e7a738785b17db37f3d991cc122b22f867dd8
StevieinWaterford <stevieindub@gmail.com>
2021-11-02 21:24:51 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://focevhkyiwajqc:9ffad8001352e934989e427b63cf5c71d81b9d1d93e15d1fefbef7b0af9daba7@ec2-18-202-1-222.eu-west-1.compute.amazonaws.com:5432/d73qsfvdcu0e70"
+os.environ["SECRET_KEY"] = "randomSecretKey2021Alan43!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://884173494267683:2A1aK79n30uxfpSjxtgcjAXpt_g@dzbad1rgb"
